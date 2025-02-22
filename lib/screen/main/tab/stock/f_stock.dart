c6a5a6304f1bfb6e52914ae2494e0a36e4081ff6
KamaTAEWOO <zszs2020@naver.com>
2025-02-22 19:37:08 +0900

    네비게이션 수정

---
+++ lib/screen/main/tab/stock/f_stock.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StockFragmnet extends StatefulWidget {
+  const StockFragmnet({super.key});
+
+  @override
+  State<StockFragmnet> createState() => _StockFragmnetState();
+}
+
+class _StockFragmnetState extends State<StockFragmnet> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/screen/main/tab/tab_item.dart
+++ lib/screen/main/tab/tab_item.dart
@@ -1,15 +1,18 @@
 import 'package:fast_app_base/common/common.dart';
-import 'package:fast_app_base/screen/main/tab/favorite/f_favorite.dart';
+import 'package:fast_app_base/screen/main/tab/all/f_all.dart';
+import 'package:fast_app_base/screen/main/tab/benefit/f_benefit.dart';
 import 'package:fast_app_base/screen/main/tab/home/f_home.dart';
+import 'package:fast_app_base/screen/main/tab/stock/f_stock.dart';
+import 'package:fast_app_base/screen/main/tab/ttospay/ttospay.dart';
 import 'package:flutter/material.dart';
 
 enum TabItem {
   // 라인복사 단축키 : Alt + Shift + 방향키 아래
   home(Icons.home, '홈', HomeFragment()),
-  benefit(Icons.home, '혜택', HomeFragment()),
-  tosspay(Icons.home, '토스페이', HomeFragment()),
-  stock(Icons.home, '주식', HomeFragment()),
-  alll(Icons.star, '전체', FavoriteFragment(isShowBackButton: false));
+  benefit(Icons.star, '혜택', BenefitFragment()),
+  ttosspay(Icons.payment, '토스페이', TtospayFragment()),
+  stock(Icons.candlestick_chart, '주식', StockFragmnet()),
+  all(Icons.menu, '전체', AllFragment());
 
   final IconData activeIcon;
   final IconData inActiveIcon;
