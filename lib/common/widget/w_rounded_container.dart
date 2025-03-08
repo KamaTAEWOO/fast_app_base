58a060aa41db694c26bc88b9621a2709b90c356d
KamaTAEWOO <zszs2020@naver.com>
2025-03-08 20:12:35 +0900

    feature/add search

--- lib/common/widget/w_rounded_container.dart
+++ lib/common/widget/w_rounded_container.dart
@@ -6,17 +6,20 @@ class RoundedContainer extends StatelessWidget {
   final EdgeInsets padding;
   final double radius;
   final Color? backgroundColor;
+  final EdgeInsets margin;
 
   const RoundedContainer(
       {required this.child,
       super.key,
       this.radius = 20,
       this.backgroundColor,
+      this.margin = const EdgeInsets.symmetric(horizontal: 5),
       this.padding = const EdgeInsets.symmetric(horizontal: 20, vertical: 15)});
 
   @override
   Widget build(BuildContext context) {
     return Container(
+        margin: margin,
         padding: padding,
         decoration: BoxDecoration(
             color: backgroundColor ?? context.appColors.roundedLayoutBackground,
