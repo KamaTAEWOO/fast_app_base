66445c4d9fe0b0286b2b196546b44579258838b4
KamaTAEWOO <zszs2020@naver.com>
2025-03-08 21:22:49 +0900

    feature/stock finish

--- lib/common/data/preference/prefs.dart
+++ lib/common/data/preference/prefs.dart
@@ -1,6 +1,17 @@
+import 'dart:ffi';
+
 import 'package:fast_app_base/common/data/preference/item/nullable_preference_item.dart';
+import 'package:fast_app_base/common/data/preference/item/rx_preference_item.dart';
 import 'package:fast_app_base/common/theme/custom_theme.dart';
 
 class Prefs {
   static final appTheme = NullablePreferenceItem<CustomTheme>('appTheme');
+
+  static var isPushOnRx = RxPreferenceItem<bool, RxBool>('isPushOn', false);
+
+  static var sliderPosition = RxPreferenceItem<double, RxDouble>('sliderPosition', 0.0);
+
+  static var birthday = RxPreferenceItem<DateTime, Rx<DateTime>>('birthday', DateTime.now());
+
+  static var number = RxPreferenceItem<int,RxInt>('number', 0);
 }
--- lib/screen/main/tab/stock/f_stock.dart
+++ lib/screen/main/tab/stock/f_stock.dart
@@ -2,6 +2,7 @@ import 'package:fast_app_base/common/common.dart';
 import 'package:fast_app_base/common/constants.dart';
 import 'package:fast_app_base/common/widget/w_image_button.dart';
 import 'package:fast_app_base/screen/main/tab/stock/search/s_stock_search.dart';
+import 'package:fast_app_base/screen/main/tab/stock/setting/s_setting.dart';
 import 'package:fast_app_base/screen/main/tab/stock/tab/f_my_stock.dart';
 import 'package:fast_app_base/screen/main/tab/stock/tab/f_todays_discovery.dart';
 import 'package:flutter/material.dart';
@@ -42,7 +43,7 @@ class _StockFragmnetState extends State<StockFragmnet>
             ImageButton(
               imagePath: '$basePath/icon/stock_settings.png',
               onTap: () {
-                context.showSnackbar("설정");
+                Nav.push(SettingScreen());
               },
             ),
           ],
